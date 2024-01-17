da389ff4212f4f5138df959c0309508e85d06c9a
Fareed Bakhsh <150926488+FareedBakhsh@users.noreply.github.com>
2024-01-17 23:30:49 +0500

    Add files via upload

---
+++ seven.js
@@ -0,0 +1,63 @@
+//  DOM 
+
+//Attributes 
+
+// . getAttribute(attr)// to get the attribute value
+
+// . setAttribute(attr,value)// to set the attribute val th
+
+// Style 
+// . node.style
+
+
+let div = document.querySelector("div");
+console.log(div);
+
+let name = div.getAttribute("name");
+console.log(name);
+
+
+let para = document.querySelector("p");
+
+console.log(p.getAttribute("class"));
+
+//DOM Method ;  
+
+// Insert Element ; let el = document.creatElement("div");
+
+//. node.append( el)// all adds at the end of node (inside )
+
+// . node.prepend( el) // add the start of node (inside);
+
+//>node.before( el) // add before the node (outside);
+
+//. node.after ( el) // add after the node (outside);
+
+//Delete Element
+
+//. node.remove() // remove the node 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
